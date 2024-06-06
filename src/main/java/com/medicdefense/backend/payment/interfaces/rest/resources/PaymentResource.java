803d33054ada0f47cd4ad3c30ffdc09d17cef732
Mathifaa <u20221a955@upc.edu.pe>
2024-06-06 11:48:02 -0500

    feat(payment): "add PaymentResources repository"

--- src/main/java/com/medicdefense/backend/payment/interfaces/rest/resources/PaymentResource.java
+++
@@ -1,9 +0,0 @@
-package com.medicdefense.backend.payment.interfaces.rest.resources;
-
-import java.util.Date;
-
-public record PaymentResource(int id, Date date, String legalIssue) {
-    public PaymentResource(Long id, Date date, String legalIssue, float amount, String method, Long id1) {
-        this((int) (long) id, date, legalIssue);
-    }
-}
